86456dae1e6dc3771a88cd95b8386f32f860d775
Karthik <Karthik@LAPTOP-2L4TB7K4>
2023-11-26 09:59:28 +0530

    first commit.

---
+++ Placement/src/Test.java
@@ -0,0 +1,22 @@
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Test {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","E:\\drivers\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		//EdgeDriver driver=new EdgeDriver();
+		driver.get("https://www.facebook.com/home.php");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		Thread.sleep(5000);
+		driver.close();
+		
+
+	}
+
+}
